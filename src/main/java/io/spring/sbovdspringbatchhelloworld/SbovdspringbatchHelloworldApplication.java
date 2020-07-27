fd1f78ee32da1b41b539817cefd11015f509aa71
Gustavo Gois <gustavo.gois@gmail.com>
2020-07-27 21:30:38 +0100

    Initial commit

---
+++ src/main/java/io/spring/sbovdspringbatchhelloworld/SbovdspringbatchHelloworldApplication.java
@@ -0,0 +1,13 @@
+package io.spring.sbovdspringbatchhelloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbovdspringbatchHelloworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbovdspringbatchHelloworldApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/spring/sbovdspringbatchhelloworld/SbovdspringbatchHelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.sbovdspringbatchhelloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbovdspringbatchHelloworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
