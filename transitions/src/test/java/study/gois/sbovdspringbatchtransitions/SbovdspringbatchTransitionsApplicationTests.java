8c67740bc28c32ef56cf80186106c4089c611a54
Gustavo Gois <gustavo.gois@gmail.com>
2020-07-28 22:07:35 +0100

    Transitions

---
+++ transitions/src/test/java/study/gois/sbovdspringbatchtransitions/SbovdspringbatchTransitionsApplicationTests.java
@@ -0,0 +1,13 @@
+package study.gois.sbovdspringbatchtransitions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbovdspringbatchTransitionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
